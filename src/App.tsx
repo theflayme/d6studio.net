1f59afb4b3805d28114cdb02acb0a28df43b35f6
theflayme <flaymedesign@gmail.com>
2026-02-24 11:08:30 +0200

    feat: enhance App component with social links and SVG icons
    - Updated package.json to include svgr and vite-plugin-svgr for SVG handling
    - Refactored App component to utilize SVG imports as React components
    - Added social media links with corresponding icons (GitHub, Telegram, Email)
    - Improved layout and styling of the App component for better presentation
    - Introduced new assets: Banner.jpg and avatar.jpg, while removing old PNG versions
    - Enhanced CSS styles for a more modern card layout and responsive design
    - Updated TypeScript definitions for SVG imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,88 @@
 import './styles/app.css'
-import verification from './assets/icon/verification.svg'
+import Verification from './assets/icon/verification.svg?react'
 
-import HTML from './assets/icon/html.svg'
-import CSS from './assets/icon/css.svg'
-import SCSS from './assets/icon/scss.svg'
-import Rest from './assets/icon/rest-api.svg'
-import Kubernetes from './assets/icon/kubernetes.svg'
-import JS from './assets/icon/js.svg'
-import React from './assets/icon/science.svg'
+import HTML from './assets/icon/html.svg?react'
+import CSS from './assets/icon/css.svg?react'
+import SCSS from './assets/icon/scss.svg?react'
+import Rest from './assets/icon/rest-api.svg?react'
+import Kubernetes from './assets/icon/kubernetes.svg?react'
+import JS from './assets/icon/js.svg?react'
+import ReactIcon from './assets/icon/science.svg?react'
+import GitHubIcon from './assets/icon/github.svg?react'
+import TelegramIcon from './assets/icon/telegram.svg?react'
+import EmailIcon from './assets/icon/email.svg?react'
 
 function App() {
 
+  const link = [
+    {
+      href: 'https://github.com/theflayme',
+      icon: GitHubIcon,
+    },
+    {
+      href: 'https://t.me/d6studio',
+      icon: TelegramIcon,
+    },
+    {
+      href: 'mailto:workd6studio@gmail.com',
+      icon: EmailIcon,
+    },
+  ]
   return (
     <>
-      <div className="container">
-        <header>
-          <div className="banner"></div>
-          <div className="avatar"></div>
-        </header>
-        <main>
-          <section className='title'>
+      <div className="container__card">
+        <div className="header__card">
+          <div className="header__card__banner">
+            <div className="header__card__social">
+              <ul>
+                {link.map((item, index) => (
+                  <li key={index}>
+                    <a href={item.href} target="_blank" rel="noreferrer">
+                      <item.icon className='card__social__icon' />
+                    </a>
+                  </li>
+                ))}
+              </ul>
+              <span className='header__card__button'>Follows</span>
+            </div>
+          </div>
+          <div className="header__card__avatar"></div>
+        </div>
+        <div className="content__card">
+          <div className='content__card__title'>
             <h1>Dmytro
-              <img src={verification} alt="verification" />
+              <Verification />
             </h1>
-            <h3>Front-end Developer</h3>
-          </section>
-          <section className='stacks'>
-            <img src={HTML} alt="html" />
-            <img src={CSS} alt="css" style={{ fill: "#ce6161" }} />
-            <img src={SCSS} alt="scss" />
-            <img src={Rest} alt="rest" />
-            <img src={Kubernetes} alt="kubernetes" />
-            <img src={JS} alt="js" />
-            <img src={React} alt="react" />
-          </section>
-          <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud
-            exercitation ullamco</p>
-        </main>
-      </div>
+            <h3>Front-end Developer / Designer</h3>
+          </div>
+          <div className='content__card__carusel-wrapper'>
+            <div className="carusel-visible">
+              <div className='content__card__carusel'>
+                <div className="group__carusel">
+                  <HTML className='group__carusel__item' />
+                  <CSS className='group__carusel__item' />
+                  <SCSS className='group__carusel__item' />
+                  <Rest className='group__carusel__item' />
+                  <Kubernetes className='group__carusel__item' />
+                  <JS className='group__carusel__item' />
+                  <ReactIcon className='group__carusel__item' />
+                </div>
+                <div aria-hidden className="group__carusel">
+                  <HTML className='group__carusel__item' />
+                  <CSS className='group__carusel__item' />
+                  <SCSS className='group__carusel__item' />
+                  <Rest className='group__carusel__item' />
+                  <Kubernetes className='group__carusel__item' />
+                  <JS className='group__carusel__item' />
+                  <ReactIcon className='group__carusel__item' />
+                </div>
+              </div>
+            </div >
+          </div>
+          <p className='content__card__description'>If you need a visual style for your web
+            project, I’m always happy to help</p>
+        </div >
+      </div >
     </>
   )
 }
---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+declare module '*.svg' {
+  const src: string
+  export default src
+}
+
+declare module '*.svg?react' {
+  import * as React from 'react'
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>
+  export default ReactComponent
+}

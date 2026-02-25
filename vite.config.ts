e4e2112f3c1627338c41810b5644b38f5357965f
theflayme <flaymedesign@gmail.com>
2026-02-25 07:46:25 +0200

    feat: add rollup visualizer plugin and enhance build configuration
    - Integrated rollup-plugin-visualizer for better bundle analysis in production mode.
    - Updated build settings to target 'esnext', disable sourcemaps, and use 'terser' for minification.
    - Configured manual chunking for vendor libraries (react, react-dom) to optimize bundle size.

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import { visualizer } from "rollup-plugin-visualizer";
 
-
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     svgr(),
     ...(mode === 'production' ? [cssInjectedByJsPlugin()] : []),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
   ],
+  build: {
+    target: 'esnext',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  },
 }))
